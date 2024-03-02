fca5c0368ac534e4f26b4add136ebf00d4516bf0
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-03-03 02:28:09 +0330

    Implement Session View

--- AISightQuest/Modules/Session/View/SessionView.swift
+++ AISightQuest/Modules/Session/View/SessionView.swift
@@ -16,39 +16,113 @@ struct SessionView: View {
     
     @State private var isShowingScannerSheet = false
     @State private var questionText = ""
+    @State private var attributedText = NSAttributedString(string: "")
     
     var body: some View {
-        VStack(spacing: 32) {
-            TextEditor(text: $viewModel.sessions[viewModel.sessionIndex].text)
+        VStack(spacing: 18) {
+            TextView(attributedText: $attributedText)
                 .padding()
-                .frame(maxHeight: 500)
+                .background(LinearGradient(colors: [.lilac500, .darkBlue500, .lilac400, .darkBlue600],
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing)
+                    .opacity(0.7)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                )
+                .padding()
             
             Button {
                 isShowingScannerSheet = true
             } label: {
                 Label("Scan", systemImage: "camera.viewfinder")
-                    .foregroundColor(.black)
+                    .foregroundColor(.darkBlue900)
                     .font(.title2)
             }
-            .padding(8)
+            .padding()
             .background(.lilac400)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .clipShape(RoundedRectangle(cornerRadius: 32))
+            .shadow(color: .lilac500,radius: 10)
+            .shadow(color: .darkBlue500,radius: 15)
             
-            TextField("Question Text Field", text: $questionText)
-                .padding()
+            HStack {
+                TextField("Question Text Field", text: $questionText)
+                    .submitLabel(.search)
+                    .padding([.vertical, .leading])
+                    .onSubmit {
+                        let result = searchButtonTapped(question: questionText,
+                                                        context: viewModel.sessions[viewModel.sessionIndex].text)
+                        
+                        if let attributedString = result.attributedText {
+                            attributedText = attributedString
+                        }
+                        questionText = result.questionText
+                    }
+                
+                Button {
+                    questionText = ""
+                } label: {
+                    Image(systemName: "xmark.square")
+                        .foregroundColor(.darkBlue500)
+                        .font(.title2)
+                        .padding(.trailing)
+                }
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(.lilac200, lineWidth: 2)
+            )
+            .padding()
+        }
+        .onChange(of: attributedText.string) { _, newValue in
+            viewModel.sessions[viewModel.sessionIndex].text = newValue
+        }
+        .onAppear {
+            setAttributedText(viewModel.sessions[viewModel.sessionIndex].text)
         }
         .sheet(isPresented: $isShowingScannerSheet) { makeScannerView().ignoresSafeArea() }
     }
-     
+    
+    private func setAttributedText(_ text: String) {
+        attributedText = NSMutableAttributedString(string: text,
+                                                   attributes: [.foregroundColor: UIColor.black,
+                                                                .font: UIFontMetrics(forTextStyle: .body)
+                                                                .scaledFont(for: UIFont.systemFont(ofSize: 17))])
+    }
+    
     private func makeScannerView() -> ScannerView {
         ScannerView { textPerPage in
             if let text = textPerPage?
                 .joined(separator: "\n")
                 .trimmingCharacters(in: .whitespacesAndNewlines) {
-                viewModel.sessions[viewModel.sessionIndex].text = text
+                setAttributedText(text)
             }
             isShowingScannerSheet = false
         }
+    }
+    
+    // TODO: Should be Move to The View Model
+    private func searchButtonTapped(question: String, context: String) -> (attributedText: NSAttributedString?, questionText: String){
+        // Use the BERT model to search for the answer.
+        let answer = BERT().findAnswer(for: question, in: context)
+        
+        // Highlight the answer substring in the original text.
+        var attributedText: NSAttributedString?
+        if answer.base == context {
+            let mutableAttributedText = NSMutableAttributedString(string: context,
+                                                                  attributes: [.foregroundColor: UIColor.black,
+                                                                               .font: UIFontMetrics(forTextStyle: .body)
+                                                                               .scaledFont(for: UIFont.systemFont(ofSize: 17))])
+            
+            let location = answer.startIndex.utf16Offset(in: context)
+            let length = answer.endIndex.utf16Offset(in: context) - location
+            let answerRange = NSRange(location: location, length: length)
+            
+            mutableAttributedText.addAttributes([.foregroundColor: UIColor.darkBlue600,
+                                                 .font: UIFontMetrics(forTextStyle: .body)
+                                                 .scaledFont(for: UIFont.boldSystemFont(ofSize: 17))],
+                                                range: answerRange)
+            attributedText = mutableAttributedText
+        }
+        return (attributedText, String(answer))
     }
 }
 
