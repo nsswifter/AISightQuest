74acbaa0499e48f515990e4027d5ad80a02dcafe
Mehdi Karami <karami.mehdi.work@github.com>
2024-03-03 02:05:12 +0330

    Add Text View Common View

---
+++ AISightQuest/Common/View/TextView.swift
@@ -0,0 +1,53 @@
+//
+//  TextView.swift
+//  AISightQuest
+//
+//  Created by Mehdi Karami on 3/3/24.
+//
+
+import SwiftUI
+
+// MARK: - Text View
+
+struct TextView: UIViewRepresentable {
+    @Binding var attributedText: NSAttributedString
+    
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.delegate = context.coordinator
+        textView.autocapitalizationType = .sentences
+        textView.isSelectable = true
+        textView.isUserInteractionEnabled = true
+        textView.backgroundColor = .clear
+        textView.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.systemFont(ofSize: 17))
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.attributedText = attributedText
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(innerAttributedText: $attributedText)
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        @Binding var innerAttributedText: NSAttributedString
+        
+        init(innerAttributedText: Binding<NSAttributedString>) {
+            self._innerAttributedText = innerAttributedText
+        }
+        
+        func textViewDidChangeSelection(_ textView: UITextView) {
+            innerAttributedText = textView.attributedText
+        }
+    }
+}
+
+// MARK: - Text View Preview
+
+#if DEBUG
+#Preview {
+    TextView(attributedText: .constant(NSAttributedString(string: "Hello World")))
+}
+#endif
