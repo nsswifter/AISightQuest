0c778092d146af7ce5593b3aee6da96076157584
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-03-11 23:44:49 +0330

    Add Intro Model

---
+++ AISightQuest/Modules/Intro/Model/Intro.swift
@@ -0,0 +1,27 @@
+//
+//  Intro.swift
+//  AISightQuest
+//
+//  Created by Mehdi Karami on 3/3/24.
+//
+
+import SwiftUI
+
+struct Intro {
+    struct IntroItem: Identifiable {
+        var id: String = UUID().uuidString
+        var imageName: String
+        var title: String
+        var text: String
+    }
+    
+    struct Data {
+        static var intros: [IntroItem] = [
+            IntroItem(imageName: "AI-Sight-Quest-Lottie", title: "Sample", text: "Sample"),
+            IntroItem(imageName: "AI-Sight-Quest-Lottie", title: "Sample", text: "Sample")
+        ]
+        
+        static var firstIntro = IntroItem(imageName: "AI-Sight-Quest-Lottie", title: "Sample", text: "Sample")
+        static var lastIntro = IntroItem(imageName: "AI-Sight-Quest-Lottie", title: "Sample", text: "Sample")
+    }
+}
