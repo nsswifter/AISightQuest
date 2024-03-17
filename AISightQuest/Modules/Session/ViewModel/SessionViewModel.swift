8550fcd13fe199574e3a13f77e0216cfd1d0526c
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-03-17 22:22:03 +0330

    Move Answer Funder to View Model

--- AISightQuest/Modules/Session/ViewModel/SessionViewModel.swift
+++ AISightQuest/Modules/Session/ViewModel/SessionViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Mehdi Karami on 2/25/24.
 //
 
-import Foundation
+import SwiftUI
 import SwiftData
 
 extension SessionView {
@@ -31,6 +31,31 @@ extension SessionView {
             } catch {
                 print("Fetch failed")
             }
+        }
+        
+        func findAnswer(for question: String, in context: String) -> (attributedText: NSAttributedString?, questionText: String){
+            // Use the BERT model to search for the answer.
+            let answer = BERT().findAnswer(for: question, in: context)
+            
+            // Highlight the answer substring in the original text.
+            var attributedText: NSAttributedString?
+            if answer.base == context {
+                let mutableAttributedText = NSMutableAttributedString(string: context,
+                                                                      attributes: [.foregroundColor: UIColor.black,
+                                                                                   .font: UIFontMetrics(forTextStyle: .body)
+                                                                                   .scaledFont(for: UIFont.systemFont(ofSize: 17))])
+                
+                let location = answer.startIndex.utf16Offset(in: context)
+                let length = answer.endIndex.utf16Offset(in: context) - location
+                let answerRange = NSRange(location: location, length: length)
+                
+                mutableAttributedText.addAttributes([.foregroundColor: UIColor.darkBlue600,
+                                                     .font: UIFontMetrics(forTextStyle: .body)
+                                                     .scaledFont(for: UIFont.boldSystemFont(ofSize: 17))],
+                                                    range: answerRange)
+                attributedText = mutableAttributedText
+            }
+            return (attributedText, String(answer))
         }
     }
 }
