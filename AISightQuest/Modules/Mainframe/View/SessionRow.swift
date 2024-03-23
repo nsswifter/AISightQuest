01d2bea93c97e1ad868235684ea33775029f3a25
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-03-23 22:42:01 +0330

    Select All Texts On Rename Session

--- AISightQuest/Modules/Mainframe/View/SessionRow.swift
+++ AISightQuest/Modules/Mainframe/View/SessionRow.swift
@@ -15,27 +15,35 @@ struct SessionRow: View {
 
     var body: some View {
         HStack {
-            Image(systemName: shouldRename ? "square.and.pencil.circle" : "doc.viewfinder.fill")
+            Image(systemName: shouldRename ? "pencil" : "doc.text.image")
+                .contentTransition(.symbolEffect(.replace))
                 .foregroundStyle(.lilac400)
                 .font(.title3)
-
-            if shouldRename {
-                TextField("new session", text: $session.name)
-                    .foregroundStyle(.lilac100)
-                    .focused($isTextFieldFocused)
-                    .submitLabel(.done)
-                    .onSubmit() {
-                        shouldRename = false
-                    }
-            } else {
-                Text(session.name)
-                    .foregroundStyle(.lilac100)
-                    .contextMenu {
-                        Button("rename") {
-                            shouldRename = true
-                            isTextFieldFocused = true
+            
+            Group {
+                if shouldRename {
+                    TextField("new session", text: $session.name)
+                        .tint(.lilac400)
+                        .focused($isTextFieldFocused)
+                        .submitLabel(.done)
+                        .onSubmit() {
+                            shouldRename = false
                         }
-                    }
+                } else {
+                    Text(session.name)
+                        .contextMenu {
+                            Button("rename") {
+                                shouldRename = true
+                                isTextFieldFocused = true
+                            }
+                        }
+                }
+            }
+            .foregroundStyle(.lilac100)
+            .onChange(of: isTextFieldFocused) {
+                if $1 {
+                    UIApplication.shared.sendAction(#selector(UIResponder.selectAll(_:)), to: nil, from: nil, for: nil)
+                }
             }
         }
         .onKeyboardHide {
