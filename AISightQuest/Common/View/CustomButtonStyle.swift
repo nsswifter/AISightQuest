f6292e7826b8e485a32f5b8550c24dbd15299228
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-03-29 02:46:59 +0330

    Add Custom Button Style

---
+++ AISightQuest/Common/View/CustomButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonStyle.swift
+//  AISightQuest
+//
+//  Created by Mehdi Karami on 3/29/24.
+//
+
+import SwiftUI
+
+struct CustomButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonStyle()
+}
