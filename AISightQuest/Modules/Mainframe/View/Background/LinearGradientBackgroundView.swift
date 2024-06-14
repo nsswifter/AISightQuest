03ecbac5b206dadc235e7ec8611865fabbcf8a21
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-06-14 18:22:14 +0330

    Add Linear Gradient Background View

---
+++ AISightQuest/Modules/Mainframe/View/Background/LinearGradientBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  LinearGradientBackgroundView.swift
+//  AISightQuest
+//
+//  Created by Mehdi Karami on 6/14/24.
+//
+
+import SwiftUI
+
+struct LinearGradientBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LinearGradientBackgroundView()
+}
